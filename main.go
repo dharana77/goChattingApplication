7ff99e993bf090bb8dffd2c8734c42fcd52a8e00
dharana77 <dharana7723@gmail.com>
2023-06-28 22:36:28 +0900

    fix: main 변수 global으로 변경, serve 로직 추가

--- main.go
+++ main.go
@@ -7,19 +7,32 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var clients = make(map[*websocket.Conn]bool)
+//map에서 웹소켓 연결에 대한 포인터를 정의
+var broadcast = make(chan Message)
+//클라이언트 측에서 보내온 메시지를 큐잉하는 역할 담당
+
+var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Email string `json:"email"`
+	Username string `json:"username"`
+	Message string `json:"message"`
+}
+
 func main(){
 	//채팅 서버의 메인이 되는 변수
-	var clients = make(map[*websockets.Conn]bool)
-	//map에서 웹소켓 연결에 대한 포인터를 정의
-	var broadcast = make(chan Message)
-	//클라이언트 측에서 보내온 메시지를 큐잉하는 역할 담당
-	
-	//업그레이더 정의. http 연결을 받고 websocket으로 업그레이드 하는 기능을 가진 개체
-	var upgrader = websocket.Upgraders{}
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/", fs)
 	
 	http.HandleFunc("/ws", handleConnections)
-	go handleMessages()
+	go handleMessage()
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
 
+	if err != nil{
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
 //handleConnections 구현 (연결 핸들러)
@@ -28,7 +41,9 @@ func main(){
 //받으면 broadcast 채널에 보내는 방식
 
 func handleConnections(responseWriter http.ResponseWriter, request *http.Request){
-	
+	//업그레이더 정의. http 연결을 받고 websocket으로 업그레이드 하는 기능을 가진 개체
+	var upgrader = websocket.Upgrader{}
+
 	ws, err := upgrader.Upgrade(responseWriter, request, nil)
 	if err != nil{
 		log.Fatal(err)
@@ -50,13 +65,18 @@ func handleConnections(responseWriter http.ResponseWriter, request *http.Request
 	}
 }
 
-
-
-
-
-
-type Message struct {
-	Email string `json:"email"`
-	Username string `json:"username"`
-	Message string `json:"message"`
+func handleMessage(){
+	for{
+		//브로드캐스트 채널에서 다음 메시지를 받는다.
+		msg := <- broadcast
+		//현재 접속 중인 클라이언트 모두에게 메시지를 보낸다.xw
+		for client := range clients{
+			err := client.WriteJSON(msg)
+			if err != nil{
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
